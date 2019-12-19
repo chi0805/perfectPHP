3f487a8b8d357b29588509976ae1c9663478c146
root <root@ip-172-31-37-105.ap-northeast-1.compute.internal>
2019-12-19 09:26:46 +0000

    description add

--- chapter5/lateStaticBindings.php
+++ chapter5/lateStaticBindings.php
@@ -1,11 +1,13 @@
 <?php
+//遅延静的束縛とは、継承されたクラスのメソッドなどを親クラスで解決するめの機能
+
 class Foo{
 	public function helloGateway(){
 //		self::hello();  
-//		自分自身を親に持つクラスのメソッドを参照できず、子クラスのオブジェクトでもhelloGateway()メソッドを呼び出した結果は、
-//              'Foo hello!'になってしまう
+//			自分自身を親に持つクラスのメソッドを参照できず、子クラスのオブジェクトでもhelloGateway()メソッドを呼び出した結果は、
+//              	'Foo hello!'になってしまう
 		static::hello();
-//              staticキーワードを使用することで、親クラスFooのhelloGateway()メソッドから子クラスBarのhello()メソッドを呼び出すことができる
+//              	staticキーワードを使用することで、親クラスFooのhelloGateway()メソッドから子クラスBarのhello()メソッドを呼び出すことができる
 	}
 
 	public static function hello(){
@@ -15,7 +17,7 @@ class Foo{
 
 class Bar extends Foo {
 	public static function hello(){
-		echo __CLASS__, 'hello', PHP_EOL;
+		echo __CLASS__, ' hello!', PHP_EOL;
 	}
 }
 
