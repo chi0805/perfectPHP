bab381e46f21fa5131ef055ff7032131577754c9
root <root@ip-172-31-37-105.ap-northeast-1.compute.internal>
2019-12-24 04:13:06 +0000

    ClassLoader.php add

---
+++ chapter7/application/core/ClassLoader.php
@@ -0,0 +1,32 @@
+<?php
+class ClassLoader{
+        protected $dirs;
+
+        //PHPにオートローダークラスを登録する処理。
+        //sql_autoload_register()関数に設定したコールバックが、オートロード時に呼び出される。
+        public function register(){
+                sql_autoload_register(array($this, 'loadClass'));
+        }
+        
+        //このメソッドの引数には、オートロードの対象とするディレクトリへのフルパスを指定する。
+        public function registerDir($dir){
+                $this->dirs[] = $dir;
+        }
+        
+        //このメソッドは、オートロード時にPHPから自動的に呼び出され、クラスファイルの読み込みを行う。
+        public function loadClass($class){
+                //クラス名のファイルを探し、そのファイルが見つかればrequireで読み込む。
+                foreach($this->dirs as $dir){
+                        $file = $dir . '/' . $class . '.php';
+                        
+                        if (is_readable($file)){
+                                require $file;
+                                
+                                return;
+                        }
+                }
+        }
+} 
+
+
+?>
